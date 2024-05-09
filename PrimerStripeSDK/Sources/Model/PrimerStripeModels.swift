ba2ca8638a906623e97ef691d609d03bec11f5b9
StefanV-PRIMERIO <stefan.vrancianu@primer.io>
2024-05-09 12:24:11 +0300

    chore: update public PrimerStripeParams

--- PrimerStripeSDK/Sources/Model/PrimerStripeModels.swift
+++ PrimerStripeSDK/Sources/Model/PrimerStripeModels.swift
@@ -9,11 +9,19 @@ import Foundation
 
 // MARK: - PrimerStripeParams
 public struct PrimerStripeParams {
-    var publishableKey: String
-    var clientSecret: String
-    var returnUrl: String
-    var fullName: String
-    var emailAddress: String
+    public var publishableKey: String
+    public var clientSecret: String
+    public var returnUrl: String
+    public var fullName: String
+    public var emailAddress: String
+    
+    public init(publishableKey: String, clientSecret: String, returnUrl: String, fullName: String, emailAddress: String) {
+        self.publishableKey = publishableKey
+        self.clientSecret = clientSecret
+        self.returnUrl = returnUrl
+        self.fullName = fullName
+        self.emailAddress = emailAddress
+    }
 }
 
 // MARK: - PrimerStripeStatus
