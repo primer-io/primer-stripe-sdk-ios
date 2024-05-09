3947acc930568b4842b28c5a41f7a2f6e921da23
StefanV-PRIMERIO <stefan.vrancianu@primer.io>
2024-05-09 11:05:57 +0300

    chore: update build frameworks with the implementation

--- Frameworks/PrimerStripeSDK.xcframework/ios-arm64_x86_64-simulator/PrimerStripeSDK.framework/Headers/PrimerStripeSDK-Swift.h
+++ Frameworks/PrimerStripeSDK.xcframework/ios-arm64_x86_64-simulator/PrimerStripeSDK.framework/Headers/PrimerStripeSDK-Swift.h
@@ -277,6 +277,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import UIKit;
 #endif
 
 #endif
@@ -297,6 +298,18 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class NSCoder;
+@class NSString;
+@class NSBundle;
+
+SWIFT_CLASS("_TtC15PrimerStripeSDK35PrimerStripeCollectorViewController")
+@interface PrimerStripeCollectorViewController : UIViewController
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
+- (void)viewDidLoad;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil SWIFT_UNAVAILABLE;
+@end
+
+
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -584,6 +597,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_warning("-Watimport-in-framework-header")
 #pragma clang diagnostic ignored "-Watimport-in-framework-header"
 #endif
+@import UIKit;
 #endif
 
 #endif
@@ -604,6 +618,18 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+@class NSCoder;
+@class NSString;
+@class NSBundle;
+
+SWIFT_CLASS("_TtC15PrimerStripeSDK35PrimerStripeCollectorViewController")
+@interface PrimerStripeCollectorViewController : UIViewController
+- (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder OBJC_DESIGNATED_INITIALIZER;
+- (void)viewDidLoad;
+- (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil SWIFT_UNAVAILABLE;
+@end
+
+
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
